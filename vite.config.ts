a113f5d1c0d158ac1c5053bc7f31caffad388855
Fercali777 <fernandocalixto89@gmail.com>
2025-03-20 15:10:39 +0100

    filters and charge content All fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
   server: {
     proxy: {
-      '/discogs': {
+      '/api/discogs': {
         target: 'https://api.discogs.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/discogs/, ''),
+        rewrite: (path) => path.replace(/^\/api\/discogs/, ''),
       },
     },
   },
